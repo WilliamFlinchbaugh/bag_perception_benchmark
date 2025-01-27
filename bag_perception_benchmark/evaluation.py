f8ec6e08a275de2eec0179c38eb6364980335ffc
William Flinchbaugh <williamflinchbaugh@gmail.com>
2025-01-26 22:13:35 -0600

    evaluate bags by directory

--- bag_perception_benchmark/evaluation.py
+++ bag_perception_benchmark/evaluation.py
@@ -9,13 +9,10 @@ import os
 
 # parse the arguments
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--bag_file", type=str, required=True)
-argparser.add_argument("--run_id", type=str, required=True)
-argparser.add_argument("--output_df", type=str, required=True)
+argparser.add_argument("--bag_dir", type=str, required=True)
 args = argparser.parse_args()
-bag_file = args.bag_file
-run_id = args.run_id
-output_df_path = args.output_df
+bag_dir = args.bag_dir
+output_df_path = os.path.join(bag_dir, "metrics.csv")
 
 # topic to read from for the detected objects
 detected_objs_topic = "/perception/object_recognition/detection/objects"
@@ -23,89 +20,95 @@ detected_objs_topic = "/perception/object_recognition/detection/objects"
 # topic to read from for the ground truth objects
 gt_topic = "/simulation/entity/status"
 
-# read the bag
-reader = SequentialReader()
-storage_options = StorageOptions(uri=bag_file, storage_id="sqlite3")
-converter_options = ConverterOptions(input_serialization_format="cdr", output_serialization_format="cdr")
-reader.open(storage_options, converter_options)
+bags = os.listdir(bag_dir)
+bags = [bag for bag in bags if bag.endswith(".db3")]
 
-# filter to only read the topics we care about
-filter = StorageFilter(topics=[detected_objs_topic, gt_topic, "/tf"])
-reader.set_filter(filter)
-
-# get the bag metadata
-topic_types = reader.get_all_topics_and_types()
-
-# get the type string to type mapping
-typestr_to_type = {}
-for meta_info in topic_types:
-    typestr_to_type[meta_info.name] = locate(meta_info.type.replace("/", "."))
-
-# read until we get the first predicted objects message
-predicted_objects_msgs = []
-gt_objects_msg = None
-car_positions = []
-objects_seen = set()
-while reader.has_next():
-    (topic, msg, t) = reader.read_next()
-    msg = deserialize_message(msg, typestr_to_type[topic])
-    if topic == gt_topic:
-        gt_objects_msg = msg
+for bag in bags:
     
-    elif topic == "/tf":
-        # save the current position of the base link for later use
-        for transform in msg.transforms:
-            if transform.header.frame_id == "map" and transform.child_frame_id == "base_link":
-                car_positions.append(transform)
-    
-    elif topic == detected_objs_topic:
-        # append the message to the list
-        # also keep track of the index of the car's last position for matching
-        predicted_objects_msgs.append((msg, car_positions[-1]))
+    bag_file = os.path.join(bag_dir, bag)
 
-# range to consider a ground truth object as able to be detected
-# if within this range and not detected, it is a false negative
-detection_range = 20
+    # read the bag
+    reader = SequentialReader()
+    storage_options = StorageOptions(uri=bag_file, storage_id="sqlite3")
+    converter_options = ConverterOptions(input_serialization_format="cdr", output_serialization_format="cdr")
+    reader.open(storage_options, converter_options)
 
-# convert the messages to DetectionObject objects and create frames for the predicted objects
-gt_objects = set()
-frames = []
-for pred_objs, car_pos in predicted_objects_msgs:
-    pred_objs = [DetectionObject().init_with_pred(pred_obj, car_pos) for pred_obj in pred_objs.objects]
-    
-    # store pred_objs for frame, and gt_objs for frame, and the ego vehicle's position
-    frames.append((pred_objs, car_pos.transform.translation))
+    # filter to only read the topics we care about
+    filter = StorageFilter(topics=[detected_objs_topic, gt_topic, "/tf"])
+    reader.set_filter(filter)
 
-# convert the ground truth objects to DetectionObject objects
-gt_objects = [DetectionObject().init_with_gt(gt_obj) for gt_obj in gt_objects_msg.data]
+    # get the bag metadata
+    topic_types = reader.get_all_topics_and_types()
 
-# objects to ignore when calculating metrics
-ignore_objects = ["Pedestrian1", "Van0"]
+    # get the type string to type mapping
+    typestr_to_type = {}
+    for meta_info in topic_types:
+        typestr_to_type[meta_info.name] = locate(meta_info.type.replace("/", "."))
 
-# get metrics for the frames
-results_df = calculate_metrics(frames, gt_objects, detection_range)
+    # read until we get the first predicted objects message
+    predicted_objects_msgs = []
+    gt_objects_msg = None
+    car_positions = []
+    objects_seen = set()
+    while reader.has_next():
+        (topic, msg, t) = reader.read_next()
+        msg = deserialize_message(msg, typestr_to_type[topic])
+        if topic == gt_topic:
+            gt_objects_msg = msg
+        
+        elif topic == "/tf":
+            # save the current position of the base link for later use
+            for transform in msg.transforms:
+                if transform.header.frame_id == "map" and transform.child_frame_id == "base_link":
+                    car_positions.append(transform)
+        
+        elif topic == detected_objs_topic:
+            # append the message to the list
+            # also keep track of the index of the car's last position for matching
+            predicted_objects_msgs.append((msg, car_positions[-1]))
 
-# print the metrics
-metrics_summary(results_df)
+    # range to consider a ground truth object as able to be detected
+    # if within this range and not detected, it is a false negative
+    detection_range = 20
 
-# record the metrics in a pandas dataframe
-# if the file does not exist, create it
-# otherwise, append to it
-if not os.path.exists(output_df_path):
-    df = pd.DataFrame(columns=["run_id"] + list(results_df.columns))
-else:
-    df = pd.read_csv(output_df_path)
+    # convert the messages to DetectionObject objects and create frames for the predicted objects
+    gt_objects = set()
+    frames = []
+    for pred_objs, car_pos in predicted_objects_msgs:
+        pred_objs = [DetectionObject().init_with_pred(pred_obj, car_pos) for pred_obj in pred_objs.objects]
+        
+        # store pred_objs for frame, and gt_objs for frame, and the ego vehicle's position
+        frames.append((pred_objs, car_pos.transform.translation))
 
-# add the run_id column to the results dataframe
-results_df["run_id"] = run_id
+    # convert the ground truth objects to DetectionObject objects
+    gt_objects = [DetectionObject().init_with_gt(gt_obj) for gt_obj in gt_objects_msg.data]
 
-# concat the new metrics to the dataframe
-df = pd.concat([df, results_df])
+    # objects to ignore when calculating metrics
+    ignore_objects = ["Pedestrian1", "Van0"]
 
-# write the dataframe to the output file
-df.to_csv(output_df_path, index=False)
+    # get metrics for the frames
+    results_df = calculate_metrics(frames, gt_objects, detection_range)
 
-print(f"Metrics written to {output_df_path}")
+    # print the metrics
+    metrics_summary(results_df)
 
-print("Creating animation...")
-create_animation(frames, gt_objects)
+    # record the metrics in a pandas dataframe
+    # if the file does not exist, create it
+    # otherwise, append to it
+    if not os.path.exists(output_df_path):
+        df = pd.DataFrame(columns=["run_id"] + list(results_df.columns))
+    else:
+        df = pd.read_csv(output_df_path)
+
+    run_id = os.path.basename(bag_file).split(".")[0].replace("_output_0", "")
+
+    # add the run_id column to the results dataframe
+    results_df["run_id"] = run_id
+
+    # concat the new metrics to the dataframe
+    df = pd.concat([df, results_df])
+
+    # write the dataframe to the output file
+    df.to_csv(output_df_path, index=False)
+
+    print(f"Metrics written to {output_df_path} for run {run_id}")
